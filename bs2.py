encrypt & decrypt - returns wrong results

bs2.py
@@ -3,71 +3,74 @@ import custom_types as T
 from constants import N, Q, P
 
 
-def BS2POLq(bytes: T.Bytes) -> T.Poly:
-    out = np.zeros((N,), dtype=np.uint16)
+def BS2POLx(x):
+    def func(bytes: T.Bytes) -> T.Poly:
+        out = np.zeros((N,), dtype=np.uint16)
 
-    bytes_last = Q*N//8-1
-    for i in range(N):
-        start_byte = bytes_last - (i*Q) // 8
-        start_offset = (i*Q) % 8
-        end_byte = bytes_last - ((i+1)*Q) // 8
-        end_offset = 8 - ((i+1)*Q) % 8
-        is_3_bytes = (start_byte - end_byte) > 1
-        
-        out[i] = (bytes[start_byte] & (0xff << start_offset)) >> start_offset
-        out[i] |= (bytes[start_byte-1] << (8-start_offset)) if is_3_bytes else 0
-        out[i] |= (bytes[end_byte] & (0xff >> end_offset)) << ((8-start_offset) + 8*is_3_bytes)
-    return out
+        bytes_last = x*N//8-1
+        for i in range(N):
+            start_byte = bytes_last - (i*x) // 8
+            start_offset = (i*x) % 8
+            end_byte = bytes_last - ((i+1)*x) // 8
+            end_offset = 8 - ((i+1)*x) % 8
+            is_3_bytes = (start_byte - end_byte) > 1
+            
+            out[i] = (bytes[start_byte] & (0xff << start_offset)) >> start_offset
+            out[i] |= (bytes[start_byte-1] << (8-start_offset)) if is_3_bytes else 0
+            out[i] |= (bytes[end_byte] & (0xff >> end_offset)) << ((8-start_offset) + 8*is_3_bytes)
+        return out
+    return func
 
-def BS2OLVECq(bytes: T.Bytes) -> T.PolyVector:
-    l = bytes.shape[0]
-    out = np.zeros((l,N), dtype=np.uint16)
-    for i in range(l):
-        out[i, :] = BS2POLq(bytes[l-i-1, :])
-    return out
+BS2POLp = BS2POLx(P)
+BS2POL2 = BS2POLx(2)
+BS2POLq = BS2POLx(Q)
 
-def POLq2BS(p: T.Poly) -> T.Bytes:
-    out = np.zeros((Q*N//8,), dtype=np.uint8)
+def BS2POLVECx(x):
+    def func(bytes: T.Bytes) -> T.PolyVector:
+        bytes = bytes.reshape((-1, x*N//8))
+        l = bytes.shape[0]
+        out = np.zeros((l,N), dtype=np.uint16)
+        bs2pol_x = BS2POLx(x)
+        for i in range(l):
+            out[i, :] = bs2pol_x(bytes[l-i-1, :])
+        return out
+    return func
 
-    bytes_last = Q*N//8-1
-    for i in range(N):
-        start_byte = bytes_last - (i*Q) // 8
-        start_offset = (i*Q) % 8
-        end_byte = bytes_last - ((i+1)*Q) // 8
-        end_offset = 8 - ((i+1)*Q) % 8
-        is_3_bytes = (start_byte - end_byte) > 1
+BS2POLVECp = BS2POLVECx(P)
+BS2POLVECq = BS2POLVECx(Q)
 
-        out[end_byte] |= ((p[i] & 0xff00) >> (8 + (8 - end_offset)))
-        out[end_byte+1] |= ((p[i] >> start_offset) & 0xff) if is_3_bytes else 0
-        out[start_byte] |= ((p[i] & (0xff >> start_offset)) << start_offset)
-    return out
+def POLx2BS(x):
+    def func(p: T.Poly) -> T.Bytes:
+        out = np.zeros((x*N//8,), dtype=np.uint8)
 
-def POLVECq2BS(v: T.PolyVector) -> T.Bytes:
-    l = v.shape[0]
-    out = np.zeros((l*Q*N//8,), dtype=np.uint8)
-    for i in range(l):
-        out[i*Q*N//8:(i+1)*Q*N//8] = POLq2BS(v[i, :])
-    return out
+        bytes_last = x*N//8-1
+        for i in range(N):
+            start_byte = bytes_last - (i*x) // 8
+            start_offset = (i*x) % 8
+            end_byte = bytes_last - ((i+1)*x) // 8
+            end_offset = 8 - ((i+1)*x) % 8
+            is_3_bytes = (start_byte - end_byte) > 1
 
-def POLp2BS(p: T.Poly) -> T.Bytes:
-    out = np.zeros((P*N//8,), dtype=np.uint8)
+            out[end_byte] |= ((p[i] & 0xff00) >> (8 + (8 - end_offset)))
+            if is_3_bytes:
+                out[end_byte+1] |= ((p[i] >> start_offset) & 0xff)
+            out[start_byte] |= ((p[i] & (0xff >> start_offset)) << start_offset)
+        return out
+    return func
 
-    bytes_last = P*N//8-1
-    for i in range(N):
-        start_byte = bytes_last - (i*P) // 8
-        start_offset = (i*P) % 8
-        end_byte = bytes_last - ((i+1)*P) // 8
-        end_offset = 8 - ((i+1)*P) % 8
-        is_3_bytes = (start_byte - end_byte) > 1
+POLq2BS = POLx2BS(Q)
+POLp2BS = POLx2BS(P)
+POL2BS = POLx2BS(2)
 
-        out[end_byte] |= ((p[i] & 0xff00) >> (8 + (8 - end_offset)))
-        out[end_byte+1] |= ((p[i] >> start_offset) & 0xff) if is_3_bytes else 0
-        out[start_byte] |= ((p[i] & (0xff >> start_offset)) << start_offset)
-    return out
+def POLVECx2BS(x):
+    def func(v: T.PolyVector) -> T.Bytes:
+        l = v.shape[0]
+        out = np.zeros((l*x*N//8,), dtype=np.uint8)
+        pol_x_2bs = POLx2BS(x)
+        for i in range(l):
+            out[i*x*N//8:(i+1)*x*N//8] = pol_x_2bs(v[i, :])
+        return out
+    return func
 
-def POLVECp2BS(v: T.PolyVector) -> T.Bytes:
-    l = v.shape[0]
-    out = np.zeros((l*P*N//8,), dtype=np.uint8)
-    for i in range(l):
-        out[i*P*N//8:(i+1)*P*N//8] = POLp2BS(v[i, :])
-    return out
+POLVECq2BS = POLVECx2BS(Q)
+POLVECp2BS = POLVECx2BS(P)

poly.py
@@ -1,14 +1,14 @@
 import numpy as np
 import custom_types as T
 from constants import PARAMS, N, Q
-from bs2 import BS2OLVECq
+from bs2 import BS2POLVECq
 from hash import shake128
 
 def gen_matrix(seed: T.Bytes, params: PARAMS) -> T.PolyMatrix:
     buf = shake128(seed, params.SABER_L*params.SABER_L*Q*N//8).reshape((params.SABER_L, params.SABER_L, Q*N//8))
     A = np.zeros((params.SABER_L, params.SABER_L, N), dtype=np.uint16)
     for i in range(params.SABER_L):
-        A[i, :, :] = BS2OLVECq(buf[params.SABER_L-i-1, :, :])
+        A[i, :, :] = BS2POLVECq(buf[params.SABER_L-i-1, :, :])
     return A
 
 def hamming(bits: np.uint8):
@@ -39,6 +39,12 @@ def shiftright(pol: T.Poly, s: int) -> T.Poly:
     out = pol.copy()
     for i in range(N):
         out[i] >>= s
+    return out
+
+def shiftleft(pol: T.Poly, s: int) -> T.Poly:
+    out = pol.copy()
+    for i in range(N):
+        out[i] <<= s
     return out
 
 #print(gen_matrix(randombytes(LIGHT_PARAMS.SABER_SEEDBYTES), LIGHT_PARAMS).tolist())

poly_mul.py
@@ -1,6 +1,6 @@
 import numpy as np
 import custom_types as T
-from constants import PARAMS
+from constants import PARAMS, N
 
 
 def poly_mul(a: T.Poly, b: T.Poly, q: int, params: PARAMS) -> T.Poly:
@@ -17,3 +17,9 @@ def matrix_vector_mul(A: T.PolyMatrix, v: T.PolyVector, q: int, transpose: bool,
                 c += poly_mul(A[i, j, :], v[j, :], q, params)
         out[i, :] = c % q
     return out
+
+def inner_prod(a: T.PolyVector, b: T.PolyVector, q: int, params: PARAMS) -> T.Poly:
+    out = np.zeros((N,), dtype=np.uint16)
+    for i in range(params.SABER_L):
+        out += poly_mul(a[i, :], b[i, :], q, params)
+    return out % q