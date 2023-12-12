175c77e9a4b5143158124462989fdf7f12c3a473
PiotrMakarewicz <makarepio@gmail.com>
2023-12-12 20:38:14 +0100

    add commented fixed random bytes

--- rng.py
+++ rng.py
@@ -3,3 +3,10 @@ import custom_types as T
 
 def randombytes(n: int) -> T.Bytes:
     return np.random.randint(0, 2**8, size=(n,))
+# RANDOM_BYTES_HARDCODED = True
+
+# def randombytes(n: int) -> T.Bytes:
+#     if RANDOM_BYTES_HARDCODED:
+#         return np.ones(size=(n,))
+#     else:
+#         return np.random.randint(0, 2**8, size=(n,))
